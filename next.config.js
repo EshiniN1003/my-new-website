e2b83a8c711b762fc57db2a3ef0eeffc30ed957d
EshiniN1003 <eshini.nethmini1003@gmail.com>
2024-11-03 20:58:22 +0530

    new

--- next.config.js
+++ next.config.js
@@ -1,11 +1,25 @@
-const path = require('path')
- 
-module.exports = {
+const path = require('path');
+
+const nextConfig = {
+  output: 'export',
+  assetPrefix: '/my-new-website/', // Replace with your repository name
+  basePath: '/my-new-website',      // Same as above
+  trailingSlash: true,               // Optional: helps with routing
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
   images: {
-    domains: ['media.dev.to', 'avatars.githubusercontent.com', 'github-readme-stats.vercel.app', 'github-readme-streak-stats.herokuapp.com', 'stardev.io', 'github-profile-summary-cards.vercel.app', 'github-profile-trophy.vercel.app'],
+    domains: [
+      'media.dev.to',
+      'avatars.githubusercontent.com',
+      'github-readme-stats.vercel.app',
+      'github-readme-streak-stats.herokuapp.com',
+      'stardev.io',
+      'github-profile-summary-cards.vercel.app',
+      'github-profile-trophy.vercel.app'
+    ],
     dangerouslyAllowSVG: true,
-  }
-}
+  },
+};
+
+module.exports = nextConfig;
